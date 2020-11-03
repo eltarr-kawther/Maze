49490ce471509e3c9ea33a908157d12d48937ce0
eltarr-kawther <kawther.eltarr@laplateforme.io>
2020-11-03 16:38:51 +0100

    added doc, edited maze

--- maze.py
+++ maze.py
@@ -5,8 +5,21 @@ Created on Tue Nov  3 09:50:26 2020
 @author: straw
 """
 class Maze:
-    def __init__(self, routes):
-        self.maze = None
+    def __init__(self, n):
+        self.n = n
+        self.maze = [[True]*(self.n*2+1)]*(self.n*2+1) 
+        
+    def building_maze(self):
+        self.maze[0][0] = False
+        for i in range(0, len(self.maze)):
+            for j in range(0, len(self.maze)):
+                if i == 0 and j == 0:
+                    pass
+                else:
+                    print()
     
     def draw_maze(self):
-        return 0
+        print(self.maze)
+        
+m = Maze(2)
+m.draw_maze()
