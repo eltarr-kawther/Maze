9981a624ada298d1885f13351427dadc1d023543
eltarr-kawther <kawther.eltarr@laplateforme.io>
2020-11-03 14:23:36 +0100

    same commit as previous

--- maze.py
+++ maze.py
@@ -6,7 +6,7 @@ Created on Tue Nov  3 09:50:26 2020
 """
 class Maze:
     def __init__(self, path):
-        content = open(path,'r')
+        self.maze = None
     
     def draw_maze(self):
         return 0
