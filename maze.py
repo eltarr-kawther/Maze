bda3c9e3d08b8e5794e20614e51102a6687f5116
eltarr-kawther <kawther.eltarr@laplateforme.io>
2020-11-04 11:25:50 +0100

    check and knock down walls

--- maze.py
+++ maze.py
@@ -4,22 +4,33 @@ Created on Tue Nov  3 09:50:26 2020
 
 @author: straw
 """
-class Maze:
-    def __init__(self, n):
-        self.n = n
-        self.maze = [[True]*(self.n*2+1)]*(self.n*2+1) 
-        
-    def building_maze(self):
-        self.maze[0][0] = False
-        for i in range(0, len(self.maze)):
-            for j in range(0, len(self.maze)):
-                if i == 0 and j == 0:
-                    pass
-                else:
-                    print()
+
+class Cell:
+    def __init__(self, position):
+        self.position = position
+        self.walls =  {'N': True, 'S': True, 'E': True, 'W': True}
+        self.wall_pairs = {'N': 'S', 'S': 'N', 'E': 'W', 'W': 'E'}
     
-    def draw_maze(self):
-        print(self.maze)
+    def has_all_walls(self):
+        if False not in self.walls.values():
+            return True
+        else:
+            return False
         
-m = Maze(2)
-m.draw_maze()
+    def knock_down_wall(self, wall, next_cell):
+        self.walls[wall] = False
+        next_cell.walls[self.wall_pairs[wall]] = False
+
+#class Maze:
+    #def __init__(self, n):
+        #self.n = n
+        #self.maze = [[True]*(self.n*2+1)]*(self.n*2+1) 
+        
+    #def building_maze(self):
+        #self.maze[0][0] = False
+        #for i in range(0, len(self.maze)):
+            #for j in range(0, len(self.maze)):
+                #if i == 0 and j == 0:
+                    #pass
+                #else:
+                    #print()
