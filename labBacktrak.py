f93c447b30d6789baf3cecfbdd284d6472df22e6
houmenou-cheikh <cheikh-tidiane.houmenou@laplateformeio>
2020-11-05 23:11:08 +0100

    recursive Backtraker maze

--- labBacktrak.py
+++ labBacktrak.py
@@ -11,30 +11,19 @@ def lab(tailleLab):
 		else:
 			M.append(list((2*tailleLab)*"#"+'.'))
 	M.insert(-1,list('#.'*tailleLab + '#'))
-	M[1][0] = '.'
-	M[-2][-1] = '.'
-	
-	"""
-	nameFile = input("\033[35mVeuillez renseigner le nom du fichier contenant le labyrinthe parfait: \033[0m")
-	fichier= open(nameFile, 'a')		
-	for j in M:
-		fichier.write(j+'\n')
-	fichier.close()
-	print(len(M[3]),len(M[0]))
-	"""
-	for j in M:
-		print(j)
+	M[1][0] = '.'									#Entree
+	M[-2][-1] = '.'									#sortie
+
 	return M
 #lab(5)
-#tailleLab =int(input("\033[35mVeuillez entrez la taille du labyrinthe: \033[0m"))
 
 
 
 
 def verif_4_Murs(maze,x,y):
 
-	if x > len(maze)-2 or y > len(maze)-2:
-		print('x or y depasse les limites du labyrinthe')
+	if x > len(maze)-2 or y > len(maze)-2 or x<0 or y<0:
+		#print('x or y depasse les limites du labyrinthe')
 		return False
 	directions = {'droite':maze[x][y+1],'gauche':maze[x][y-1],'haut':maze[x-1][y],'enBas':maze[x+1][y]}
 	
@@ -48,47 +37,58 @@ def verif_4_Murs(maze,x,y):
 # ~ print(verif_4_Murs(lab(5),1,2))
 
 def resolu(maze,x=1,y=0):
-	Nb_pas=list(range(0,len(maze),2))
-	
+	Nb_pas=list(range(0,len(maze)-2,2))
 	if maze[x][y+1] == '.':
 		y += 1
-	if verif_4_Murs(maze,x+2,y)==True:
+	if maze[x+1][y] == '.':
+		x += 1
+	if verif_4_Murs(maze,x+2,y):
 		maze[x+1][y] = '.'
 		x +=2
-		
-	if verif_4_Murs(maze,x,y+2)==True:
-		maze[x][y+1] = '.'
-		y += 2
-	if verif_4_Murs(maze,x,y+2)==True:
-		maze[x][y+1] = '.'
-		y += 2
-	if verif_4_Murs(maze,x,y+2)==True:
-		maze[x][y+1] = '.'
-		y += 2
-	print(y)
-	if verif_4_Murs(maze,x,y+2)==True:
-		maze[x][y+1] = '.'
-		y +=4
-	if verif_4_Murs(maze,x-2,y)==True:
+		resolu(maze,x,y)
+
+	if verif_4_Murs(maze,x-2,y):
 		maze[x-1][y] = '.'
-		
+		x += -2
+		resolu(maze,x,y)
+				
+	if verif_4_Murs(maze,x,y+2):
+		maze[x][y+1] = '.'
+		y += 2
+		resolu(maze,x,y)	
+				
+	if verif_4_Murs(maze,x,y-2):
+		maze[x][y-1] = '.'
+		y -= 2
+		resolu(maze,x,y)
 	
-		
-		
+
 	print('******************')
 	tab = ''
 	lon = len(maze)
 	aa=list(range(len(maze)+1))
 	for i,l in enumerate(maze):
-		for m in l:
-			tab += m
+		for k,m in enumerate(l):
+			if i == len(maze)-2 and k ==len(maze)-3 :
+				print('aaaaaaaaaaaaaaaaaaaa',i)
+				m ='.'
+				tab += m
+			else:
+				tab += m
 			
 		if i in aa:
-				tab += '\n'
-	print(tab)
-			
-
+			tab += '\n'
 	
-resolu(lab(5))
+	
+	return tab
+			
+if __name__ == '__main__':
+	
+	tailleLab =int(input("\033[35mVeuillez entrez la taille du labyrinthe: \033[0m"))
 		
+		
+	nameFile = input("\033[35mVeuillez renseigner le nom du fichier contenant le labyrinthe parfait: \033[0m")
+	fichier= open(nameFile, 'w')		
+	fichier.write(resolu(lab(tailleLab)))
+	fichier.close()
 	
